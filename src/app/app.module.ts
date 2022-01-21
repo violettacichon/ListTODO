8efdd9866f91efcef62db15f6b4e1cad3c4f5f5e
Wioletta Cichon <violettacichon@gmail.com>
2022-01-21 18:35:30 +0100

    install material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {AddTaskComponent} from './add-task/add-task.component';
 import {ViewTasksComponent} from './view-tasks/view-tasks.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {ViewTasksComponent} from './view-tasks/view-tasks.component';
     ViewTasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
